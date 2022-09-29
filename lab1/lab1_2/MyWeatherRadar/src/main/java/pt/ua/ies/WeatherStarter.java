717f9c52fa0aa108cecad8cf44bfc6c109858103
RafaelGoncalvesUA <rfg@ua.pt>
2022-09-29 15:24:31 +0100

    fix lab1_2 MyWeatherRadar

---
+++ lab1/lab1_2/MyWeatherRadar/src/main/java/pt/ua/ies/WeatherStarter.java
@@ -0,0 +1,63 @@
+package pt.ua.ies;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import pt.ua.ies.ipma_client.IpmaCityForecast; //may need to adapt package name
+import pt.ua.ies.ipma_client.IpmaService;
+
+/**
+ * demonstrates the use of the IPMA API for weather forecast
+ */
+public class WeatherStarter {
+
+    //todo: should generalize for a city passed as argument
+    private static int CITY_ID = 1010500;
+
+    public static void  main(String[] args ) {
+
+        if (args.length == 0) {
+            System.err.println("Erro: execução sem argumento.");
+            System.exit(1);
+        }
+
+        try {
+            CITY_ID = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("Erro: argumento não é um inteiro.");
+            System.exit(1);
+        }
+
+        // get a retrofit instance, loaded with the GSon lib to convert JSON into objects
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ipma.pt/open-data/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // create a typed interface to use the remote API (a client)
+        IpmaService service = retrofit.create(IpmaService.class);
+        // prepare the call to remote endpoint
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID);
+
+        try {
+            Response<IpmaCityForecast> apiResponse = callSync.execute();
+            IpmaCityForecast forecast = apiResponse.body();
+
+            if (forecast != null) {
+                var firstDay = forecast.getData().listIterator().next();
+
+                System.out.printf( "\nmax temp for %s is %4.1f %n",
+                        firstDay.getForecastDate(),
+                        Double.parseDouble(firstDay.getTMax()));
+                
+                System.exit(0);
+            } else {
+                System.out.println( "No results for this request!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
